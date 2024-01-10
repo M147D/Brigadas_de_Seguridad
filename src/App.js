f423bf10c34e27703d7121939741f88f05330943
Miguel Pastuña <david.rmig@gmail.com>
2024-01-10 13:33:35 -0500

    Actulización de interfaz para el uso de composición

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-//import ContenedorGeneral from "./components/ContenedorGeneral";
-//import AdmistrarGrupos from './components/AdministrarGrupos';
+import ContenedorGeneral from "./components/ContenedorGeneral";
+import AdmistrarGrupos from './components/AdministrarGrupos';
 import EditarGrupo from './components/EditarGrupo';
+import AdministrarGrupos from "./components/AdministrarGrupos";
 
 export default function App() {
   return(
     <div>
       {/*<ContenedorGeneral titulo="Crear Grupo"/>*/}
       {/*<AdmistrarGrupos titulo="Administrar Grupo"/>*/}
-      <EditarGrupo titulo="Editar Grupo"/>
+      {/*<EditarGrupo titulo="Editar Grupo"/>*/}
+      <ContenedorGeneral titulo="asmirGrupo" contenido={<AdministrarGrupos/>}/>
     </div>
   )
 
